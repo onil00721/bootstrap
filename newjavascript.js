68717f8ccd25e6c6c9785475d20a84dfb018f30d
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-02-10 21:31:20 +0100

    21,31

--- newjavascript.js
+++ newjavascript.js
@@ -1,9 +1,18 @@
-/*
+
 document.write("fsd");
-*/
 
 
 
+$(".dwa").mouseenter(function(){
+    $(".jeden").css({"display": "block"});
+    console.log("you click");
+});
+
+
+$(".dwa").mouseout(function(){
+    $(".jeden").css({"display": "none"});
+    console.log("you click");
+});
 
               
               
