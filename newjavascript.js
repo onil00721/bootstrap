0b5e931e1cffc08cdf977d3fff5e20aada624db1
onil00721 <35064963+onil00721@users.noreply.github.com>
2018-02-11 14:39:31 +0100

    11,02,14,39

--- newjavascript.js
+++ newjavascript.js
@@ -1,19 +1,41 @@
 
-document.write("fsd");
 
 
 
-$(".dwa").mouseenter(function(){
-    $(".jeden").css({"display": "block"});
-    console.log("you click");
+$("#dw").mouseenter(function(){
+   
+    $("#jejeje").css({"height": '300px'});
+   
+    console.log("in");
 });
 
+$("#dw").mouseleave(function(){
+   
+    $("#jejeje").css({"height": '100px'});
+    console.log("out");
+});
 
+/*
 $(".dwa").mouseout(function(){
     $(".jeden").css({"display": "none"});
     console.log("you click");
 });
 
               
-              
+              */
     
+$( "button" ).click(function() {
+  
+  $( ".je" ).fadeIn( 3000, function() {
+    $( "span" ).fadeIn( 100 );
+  });
+  return false;
+});
+
+$( "button" ).click(function() {
+  $( ".je" ).fadeIn( 3000);
+  return false;
+});
+
+
+
